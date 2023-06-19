10c0b46f9680efe2d939a8dcf22a5a19f19ae5f8
MrChelizt <chelizt@gmail.com>
2023-06-19 13:38:18 +0200

    Removed non-functioning sort select

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Box, FormControl, Select, TextField } from "@mui/material";
+import { Box, MenuItem, TextField } from "@mui/material";
 
 import { searchActions } from "../redux/slices/search";
 
@@ -30,14 +30,6 @@ export default function SearchForm() {
         label="Search"
         helperText="Search product by name"
       />
-      <Box
-        sx={{ display: "flex", justifyContent: "space-between", width: "50vw" }}
-      >
-        <Box />
-        <FormControl variant="standard">
-          <Select label="Sort by"></Select>
-        </FormControl>
-      </Box>
     </Box>
   );
 }
